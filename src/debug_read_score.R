4379b977693448bcfa60b615b647db19e077afb1
mkl <mkl48@nau.edu>
2014-03-18 19:33:51 -0400

    updates

---
+++ src/debug_read_score.R
@@ -0,0 +1,9 @@
+###Output is not being built correctly.
+
+library(devtools)
+install_github('enaR',username='MKLau',branch='mkl')
+library(enaR)
+file='/Users/Aeolus/projects/packages/enaR_development/data/oyster.dat'
+type='network'
+from.file=TRUE
+warn=FALSE
