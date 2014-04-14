c21b2a908cba9408efff5400b5d8f1d1fbcdd935
mkl <mkl48@nau.edu>
2014-04-14 14:03:17 -0700

    mkl edits table4

--- src/table4.R
+++ src/table4.R
@@ -1,15 +1,13 @@
 # Example enaR Code
 # mee13 manuscirpt
-# October 11, 2013
+# Apr 14, 2014
 # ---------------------
 rm(list=ls())
 library(enaR)   # load enaR package
-
 # -- ENTER MODEL DATA -- from Dame and Patten (1981)
 # node names
 names <- c("Filter Feeders","Microbiota","Meiofauna",
                       "Deposit Feeders","Predators","Deposited Detritus")
-
 # Internal Flows of model, as matrix (oriented row to column)
 F <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0,
                0, 0, 8.1721, 0, 1.2060, 0, 0, 0, 7.2745,
@@ -17,24 +15,15 @@ F <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0,
                0.1721, 0, 0, 15.7910, 0, 4.2403, 1.9076, 0.3262, 0), ncol=6)
 rownames(F) <- names  # add node names to rows
 colnames(F) <- names  # add node names to cols
-
 # boundary flows
 inputs <- c(41.47,0, 0, 0, 0, 0)
 outputs <- c(25.1650, 5.76, 3.5794, 0.4303, 0.3594, 6.1759)
-
 # Living
 Living <- c(TRUE,TRUE,TRUE,TRUE,TRUE,FALSE)
-
 # pack the model data into the R network data object
 m <- pack(flow=F,input=inputs, respiration=outputs,output=outputs, living=Living)
 ssCheck(m)
-
-### THIS IS NO LONGER NEEDED
-# m <- read.scor("oyster.dat")  # read model data from SCOR formatted file
-# m <- balance(m)               # balance model using AVG2 algorithm (Allesina and Bondavali)
-# u <- unpack(m)                # unpack model data to illustrate components
-# attributes(u)
-
+# perform flow analysis
 F <- enaFlow(m)               # perform ENA flow analysis
 attributes(F)                  # show analysis objects created
 F$ns                          # show flow analysis network statistics
