36aee9b015c1892e58b080f79c42d8de1733080a
mkl <mkl48@nau.edu>
2014-09-16 12:09:56 -0400

    checks for edgeflow

--- src/check_enaR/check_enaR.R
+++ src/check_enaR/check_enaR.R
@@ -1,24 +1,5 @@
 ###enaR function output checking algorithm
 ###Compares the output of for the Cone Springs Model
-###MKLau 23Jul2014
+###Init: MKLau 23Jul2014
 
-library(devtools)
-install_github(username='TheSeeLab',repo='SEELab/enaR',ref='beta')
-library(enaR)
-data(enaModels)
-enaModels <- lapply(enaModels,force.balance)
-ns.beta <- lapply(enaModels,get.ns)
-dput(do.call(rbind,ns.beta),file='~/projects/packages/enaR_development/data/nsbeta.rda')
 
-q('no')
-library(devtools)
-install_github(username='TheSeeLab',repo='SEELab/enaR',ref='edgeflow')
-library(enaR)
-data(enaModels)
-enaModels <- lapply(enaModels,force.balance)
-ns.edgeflow <- lapply(enaModels,get.ns)
-dput(do.call(rbind,ns.edgeflow),file='~/projects/packages/enaR_development/data/nsedgeflow.rda')
-
-###Checks
-dget(file='~/projects/packages/enaR_development/data/nsbeta.rda')
-dget(file='~/projects/packages/enaR_development/data/nsedgeflow.rda')
